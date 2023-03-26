6d3f5b4a68ffa6aa9b72ea2f2896caf7bfe52c1f
theghostyousee <patrickserise174@gmail.com>
2023-03-26 19:26:03 +0200

    s

--- src/Components/Presale/presale.js
+++ src/Components/Presale/presale.js
@@ -114,7 +114,7 @@ function Presale() {
                 value={amount}
                 onChange={handleAmountChange}
               ></input>
-              <p className="balance-user">Balance:{balance} </p>
+              <p className="balance-user">Your contribution{balance} </p>
 
               <div className="eth-pointer">
                 <img src={eth}></img>
